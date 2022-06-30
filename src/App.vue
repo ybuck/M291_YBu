ab27cb79ea42491096f9b7a0161c9adfc1644ca0
ybuck <97731901+ybuck@users.noreply.github.com>
2022-06-30 16:41:04 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+  import TodoApp from './components/ToDos.vue'
+  export default {
+    name: 'App',
+    components: {
+      TodoApp
+    }
+  }
+</script>
+
+<template>
+  <div id="app">
+    <todo-app></todo-app>
+  </div>
+</template>
+
